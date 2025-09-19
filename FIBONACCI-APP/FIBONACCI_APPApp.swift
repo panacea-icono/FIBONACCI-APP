b79e2657e3489971df0286aa13558c40ae32a470
panacea-icono <repositorios.panacea@gmail.com>
2025-09-18 22:41:49 -0400

    Initial Commit

---
+++ FIBONACCI-APP/FIBONACCI_APPApp.swift
@@ -0,0 +1,32 @@
+//
+//  FIBONACCI_APPApp.swift
+//  FIBONACCI-APP
+//
+//  Created by Nuevo Usuario on 18/9/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FIBONACCI_APPApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FIBONACCI-APPTests/FIBONACCI_APPTests.swift
@@ -0,0 +1,16 @@
+//
+//  FIBONACCI_APPTests.swift
+//  FIBONACCI-APPTests
+//
+//  Created by Nuevo Usuario on 18/9/25.
+//
+
+import Testing
+
+struct FIBONACCI_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
